7c9da15474169f61a84eb9ef0daf656ec4a37ce7
lucasbailey <lcbailey@uvm.edu>
2025-01-22 16:18:50 -0500

    added code to admin

--- admin.py
+++ admin.py
@@ -2,4 +2,18 @@
 # Admin Program
 # Load data from JSON file to datastore
 import importlib
+import pandas as pd
+from json import loads, dumps
+
 foobar = importlib.import_module("Database-Query-Program/authentication.py")
+
+# load the original CSV file
+file_path = "vgsales.csv"
+df = pd.read_csv(file_path)
+
+result = df.to_json(orient='records')
+
+parsed = loads(result)
+dumps(parsed)
+
+print(dumps(parsed))
