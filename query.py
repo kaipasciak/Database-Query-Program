bdc9d67549079db7aee2e49610a5ff2a294db561
Kai Pasciak <kai.pasciak@gmail.com>
2025-01-30 13:27:42 -0500

    Outline for parser function

--- query.py
+++ query.py
@@ -3,7 +3,33 @@
 # Prompt user to provide query and return data
 import string
 
+# Prompt the user for input and call the query function until program manually terminated
+def parser():
+    # Boolean to continue prompting user
+    done = False
+
+    # Print welcome message
+    print("Welcome ...")
+
+    while done == False:
+        # Store user input
+        userInput = str(input("Enter query: "))
+
+        # Process Input
+        if userInput == "help":
+            pass
+        elif userInput == "quit":
+            done = True
+        else:
+            # Parsing logic
+
+            # Call query function
+
+
+
 # Takes a type, the cols requested, and conditions and returns a list of objects from the database
 def query(type: string, cols: list[string], conditions: list[tuple[string, string, string]]):
     pass
+
+parser()
 
