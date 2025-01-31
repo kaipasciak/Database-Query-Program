cdbe7d2365603309b6230dfef5df20cabd778a7d
Kai Pasciak <kai.pasciak@gmail.com>
2025-01-31 06:07:30 -0500

    Import statements and help menu

--- query.py
+++ query.py
@@ -3,6 +3,7 @@
 # Prompt user to provide query and return data
 import string
 import pyparsing as pp
+import query_engine.py
 
 # Prompt the user for input and call the query function until program manually terminated
 def parser():
@@ -15,16 +16,26 @@ def parser():
     while done == False:
         # Store user input
         userInput = str(input("Enter query: "))
-        inputList = userInput.split()
 
         # Process Input
         if userInput == "help":
-            pass
+            # Print help menu
+            print("--- Help Menu ---\n"
+                  "Enter queries in the form:\n"
+                  "\'Get {value} of {game title}\' to find the sales, release year or platform \n"
+                  "of a specific game.\n\n"
+                  "OR\n\'Get games where {condition}...\' to filter results\n"
+                  "Condition Syntax:\n"
+                  "\'where platform is {platform name}\'\n"
+                  "\'where sales is greater/less than {sale amount}\'\n"
+                  "\'released before/after {year}\'\n"
+                  "NOTE:\n - For game or platform names with multiple words, use"
+                  " double quotes.\n - Multiple conditions may be added separated"
+                  "by \'and\'.\n")
+
         elif userInput == "quit":
             done = True
         else:
-            typeParse = pp.Word(pp.alphas)  + pp.Word(pp.alphas)
-            type = typeParse.parse_string(inputList[0] + " " + inputList[1])
             # Parsing logic
             template = (pp.QuotedString('"') | pp.Word(pp.alphas))[0, 7]
             parsed = template.parseString(userInput)
@@ -32,7 +43,6 @@ def parser():
             # Error handling
 
             # Call query function
-
 
 
 parser()
